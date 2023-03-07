10c39e89b81e87c7a176bb8352e7d7bb1cb9ca13
Mooncake Sugar <60507815+Tsukina-7mochi@users.noreply.github.com>
2023-03-08 04:44:04 +0900

    implement version check

--- Assets/GlitchShader/Editor/UpdateChecker.cs
+++ Assets/GlitchShader/Editor/UpdateChecker.cs
@@ -1,8 +1,62 @@
-﻿namespace GlitchShader
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Networking;
+using UnityEngine.PlayerLoop;
+
+namespace GlitchShader
 {
-    public class UpdateChecker
+    [Serializable]
+    public class ReleaseResponse
     {
-        public static readonly Version CurrentVersion = new Version(0, 1, 0, "-alpha.2");
+        public string name;
+    }
+
+    [InitializeOnLoad]
+    public class UpdateChecker: MonoBehaviour
+    {
+        public static readonly Version CurrentVersion = new Version(0, 1, 0, "-alpha.3");
         public static string GitHubURL = "https://api.github.com/repos/Tsukina-7mochi/glitch-shader/releases/latest";
+
+        private static IEnumerator _checkUpdate(bool showLatest)
+        {
+            var req = UnityWebRequest.Get(GitHubURL);
+            yield return req.SendWebRequest();
+
+            if (req.isNetworkError || req.isHttpError || req.responseCode != 200)
+            {
+                Debug.LogError("[Glitch Shader] Failed to fetch update.");
+            }
+            else
+            {
+                var text = req.downloadHandler.text;
+                ReleaseResponse response = JsonUtility.FromJson<ReleaseResponse>(text);
+
+                var version = Version.Parse(response.name);
+                if (CurrentVersion.CompareTo(version) < 0)
+                {
+                    Debug.Log($"[Glitch Shader] Update {version.ToString()} is available.");
+                } else if (showLatest)
+                {
+                    Debug.Log("[Glitch Shader] Glitch Shader is up to date.");
+                }
+            }
+        }
+
+        private static void StartCheckUpdate(bool showLatest)
+        {
+            Util.CoroutineRunner.Run(_checkUpdate(showLatest));
+        }
+        
+        [MenuItem("Window/Glitch Shader/Check for update")]
+        static void CheckUpdate()
+        {
+            StartCheckUpdate(true);
+        }
+        static UpdateChecker()
+        {
+            StartCheckUpdate(false);
+        }
     }
 }
