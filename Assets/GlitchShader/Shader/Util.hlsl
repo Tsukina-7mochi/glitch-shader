8fc66f6e9516c31afab1101a3239fc32645c6a0d
Mooncake Sugar <60507815+Tsukina-7mochi@users.noreply.github.com>
2023-05-09 18:23:32 +0900

    re-implement shader

--- Assets/GlitchShader/Shader/Util.hlsl
+++ Assets/GlitchShader/Shader/Util.hlsl
@@ -4,3 +4,17 @@ inline fixed maxElement(fixed3 v)
 {
   return max(max(v[0], v[1]), v[2]);
 }
+
+inline float2 computeGlitch(
+    float yPos,
+    float period,
+    float minY,
+    float maxY,
+    float2 normPrjViewDir,
+    float sharpness
+) {
+    float glitchYPos = (1 - frac(_Time.y / period)) * (maxY - minY) + minY;
+
+    float2 displacement = float2(-normPrjViewDir.y, normPrjViewDir.x);
+    return (1 - saturate(abs(yPos - glitchYPos) * sharpness)) * displacement;
+}
